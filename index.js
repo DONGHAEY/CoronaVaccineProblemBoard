5d90e25014a2f546160f41189755f6c7c159c19c
DONGHAEY <dongheaey@gmail.com>
2021-12-26 02:00:16 +0900

    gogo

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 "use strict"
 
-const express = require("express")
+const {secret} = require("./secret")
 
-const fs = require("fs")
+const express = require("express")
+const mysql = require("mysql")
+// const fs = require("fs")
 
 const Port = 5000
 const app = express()
+const db = mysql.createConnection({
+    host: secret.url,
+    user: "admin",
+    password : secret.password,
+    database: "sys"
+})
+
+db.connect(err => {
+    if (err) throw err;
+    console.log("DB is connected")
+})
 
 app.use(express.json())
 
 app.post('/addIssue', (req, res) => {
     const topic = req.query.topic
     const content = req.query.content
+    const sql = `INSERT INTO post (topic, content, date) VALUES ('${topic}', '${content}', now())`
+    db.query(sql, function (err, result, fields) {
+        if (err) throw err
+        res.json({
+            isAdded: true
+        })
+    });
 })
 
 app.get('/LIST/:where', (req, res) => {
     const where = req.params.where
-})
-
-app.post('/Like/:where', (req, res) => {
-    const where = req.params.where
+    const sql = `SELECT * FROM post WHERE id = '${where}'`
+    db.query(sql, function (err, result, fields) {
+        if (err) throw err
+        const content = result[0]
+        res.json({
+            content : content
+        })
+    });
 })
 
 app.listen(Port, () => {
